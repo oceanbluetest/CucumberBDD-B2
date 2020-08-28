5de08451a0bcef8bfba678753c8f463757decd8f
oceanbluetest <oceanbluetest@gmail.com>
2020-08-27 21:39:12 -0400

    added sauce configurations

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -4,25 +4,39 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class Driver {
     private static String propertyPath = "src/test/resources/conf/configuration.properties";
     private static WebDriver driver;
 
+    private static final String sauceUser = ConfigReader.readProperty("sauceUsername", propertyPath);
+    private static final String sauceKey = ConfigReader.readProperty("sauceKey", propertyPath);
+    private static final String URL = "https://" + sauceUser + ":" + sauceKey + "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
+
+
     public static WebDriver getDriver() {
         if (driver == null) {
-            switch (ConfigReader.readProperty("browser", propertyPath)) {
-                case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    break;
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    break;
-                default:
-                    System.out.println("Invalid browser type");
+            if (ConfigReader.readProperty("executeTestLocally", propertyPath).equalsIgnoreCase("false"))
+                driver = getRemoteDriver();
+            else {
+                switch (ConfigReader.readProperty("browser", propertyPath)) {
+                    case "chrome":
+                        WebDriverManager.chromedriver().setup();
+                        driver = new ChromeDriver();
+                        break;
+                    case "firefox":
+                        WebDriverManager.firefoxdriver().setup();
+                        driver = new FirefoxDriver();
+                        break;
+                    default:
+                        System.out.println("Invalid browser type");
+                }
             }
         }
         return driver;
@@ -31,5 +45,19 @@ public class Driver {
     public static void quitDriver(){
         getDriver().quit();
         driver = null;
+    }
+
+    public static WebDriver getRemoteDriver(){
+        WebDriver driver = null;
+        try{
+            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+            capabilities.setCapability("browserName", "chrome");
+            capabilities.setCapability("version", ConfigReader.readProperty("browser_version", propertyPath));
+            capabilities.setCapability("platform", ConfigReader.readProperty("os", propertyPath));
+            driver = new RemoteWebDriver(new URL(URL),capabilities);
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        return driver;
     }
 }
